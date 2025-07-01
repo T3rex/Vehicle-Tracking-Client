eb460760639aaa915ee712b25ac07cc31dec1819
T3rex <31337aditya@gmail.com>
2025-07-01 16:55:16 +0530

    Removed config and replaced with contants file in utils

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const LOCAL_API_URL = "http://localhost:5000/api/location";
-export const LIVE_API_URL =
-  "https://vehicle-tracking-server-3xn2.onrender.com/api/location"; // Update with your server URL
-
-export const INITIAL_POSITION = [28.590290000000003, 77.33654];
