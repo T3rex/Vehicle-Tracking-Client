c18834be5abb293188b3624303eb868b59956e44
T3rex <31337aditya@gmail.com>
2025-07-01 22:51:03 +0530

    Changed API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import { LIVE_API_URL, LOCAL_API_URL } from "./constants";
 
 export async function fetchLocation(index) {
-  const res = await fetch(`${LOCAL_API_URL}?index=${index}`);
+  const res = await fetch(`${LIVE_API_URL}?index=${index}`);
   if (res.status === 204) return null;
   const data = await res.json();
   return data;
