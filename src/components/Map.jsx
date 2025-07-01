c476d5ea445d49418ecf0fde116d376f8cbcddd0
T3rex <31337aditya@gmail.com>
2025-07-01 16:31:11 +0530

    Implemented frontend query based request model

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
 import { useEffect, useRef, useState } from "react";
 import RecenterMap from "./RecenterMap";
-import { API_URL } from "../config";
+import { LOCAL_API_URL, INITIAL_POSITION } from "../config";
 import L from "leaflet";
 import car from "../assets/Car-icon.png";
 
@@ -12,10 +12,12 @@ const carIcon = new L.Icon({
 });
 
 function MapComponent() {
-  const [position, setPosition] = useState([28.590290000000003, 77.33654]);
-  const [route, setRoute] = useState([[28.590290000000003, 77.33654]]);
+  const [position, setPosition] = useState(INITIAL_POSITION);
+  const [route, setRoute] = useState([INITIAL_POSITION]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [routeFinish, setRouteFinish] = useState(false);
   const intervalRef = useRef(null);
+  const index = useRef(0);
 
   const handlePlayPause = () => {
     if (isPlaying) {
@@ -23,13 +25,21 @@ function MapComponent() {
       intervalRef.current = null;
       setIsPlaying(false);
     } else {
+      if (routeFinish) {
+        setRoute([INITIAL_POSITION]);
+        setRouteFinish(false);
+        setPosition(INITIAL_POSITION);
+        index.current = 0;
+      }
       intervalRef.current = setInterval(async () => {
         try {
-          const res = await fetch(API_URL);
+          const res = await fetch(LOCAL_API_URL + `?index=${index.current++}`);
           if (res.status === 204) {
             clearInterval(intervalRef.current);
             intervalRef.current = null;
             setIsPlaying(false);
+            setRouteFinish(true);
+            index.current = 0;
             return;
           }
           const data = await res.json();
@@ -63,7 +73,6 @@ function MapComponent() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
-
         <Marker position={position} icon={carIcon} />
         <Polyline positions={route} color="blue" />
         <RecenterMap position={position} />
@@ -71,8 +80,7 @@ function MapComponent() {
 
       <div>
         <button className="btn" onClick={handlePlayPause}>
-          {" "}
-          {isPlaying ? "⏸ Pause" : "▶ Play"}{" "}
+          {isPlaying ? "⏸ Pause" : "▶ Play"}
         </button>
       </div>
     </>
